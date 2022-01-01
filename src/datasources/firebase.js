44c5b1127b674680337e2d665757c399e38402ce
shlee <completed0728@gmail.com>
2022-01-02 01:12:56 +0900

    Update firebase.js

--- src/datasources/firebase.js
+++ src/datasources/firebase.js
@@ -2,8 +2,8 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const oFirebase = firebase.initializeApp({
-  apiKey: //,
-  authDomain: //,
+  apiKey: "AIzaSyDFWBN-FGLlJZs6XoZfhBvD58B_ChIP-2A",
+  authDomain: "pwa-login-auth-21e52.firebaseapp.com",
 });
 
 export const oFirebaseAuth = oFirebase.auth();
